39da9522bdaa1264b579b60a7141c33869854af8
alzdirty <zackyalbaehaki@gmail.com>
2021-12-22 11:36:03 +0700

    main

--- src/List.js
+++ src/List.js
@@ -2,7 +2,7 @@ import './App.css';
 import React from 'react';
 import Done_black from './done_black_24dp.svg';
 import Delete_black from './remove_black_24dp.svg';
-import More_vert_black from './more_vert_black_24dp.svg';
+import Notes_black from './notes_black_24dp.svg';
 
   
 
@@ -15,14 +15,14 @@ function  List(props) {
         props.item.map((a, i) => 
           <div key={i} className=''>
             <div className="flex justify-between px-3 py-1" value={i}>
-            <div className="flex">
-            <button onClick={props.sudah} className="px-2" type="submit"><img src={Done_black} alt="Done_black" /></button>
-            <p>{(a.length > 15) ? a.substring(0, 10) + '...' : a }</p>
-            </div>
-            <div>
-            <button onClick={props.hapus} className="px-2" type="submit"><img src={Delete_black} alt="Delete_black" /></button>
-            <button onClick={props.detail} className="px-2" type="submit"><img src={More_vert_black} alt="More_vert_black" /></button>
-            </div>
+              <div className="flex">
+                <button onClick={props.sudah} className="px-2" type="submit"><img src={Done_black} alt="Done_black" /></button>
+                <p>{(a.length > 15) ? a.substring(0, 10) + '...' : a }</p>
+              </div>
+              <div>
+                <button onClick={props.detail} className="px-2" type="submit"><img src={Notes_black} alt="Notes_black" /></button>
+                <button onClick={props.hapus} className="px-2" type="submit"><img src={Delete_black} alt="Delete_black" /></button>
+              </div>
             </div>
             {props.tampil && (props.nilai == i)? <p className='bg-gray-50 flex py-1 px-3 flex-auto break-all w-xl'>{a}</p> : ''}
           </div>
